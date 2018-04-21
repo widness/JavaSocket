6560cfb23e51ceaad64165515c4cd5b1fc500918
Buubu <montaine@windowslive.com>
2018-04-21 17:47:30 +0200

    Fix sync bugs NewClientConnection

--- src/Client/NewClientConnection.java
+++ src/Client/NewClientConnection.java
@@ -1,26 +1,30 @@
 package client;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import controller.DataOutput;
 
 
 public class NewClientConnection {
 	private Socket clientSocket;
 	private InetAddress serverAddress;
 	private String serverIP;
-	private String clientName;
+	private String clientPseudo;
+	private String password;
+	private PrintWriter pout;
+	private DataOutput dataOutput;
 	
-	public NewClientConnection(String serverIP, String clientName) {
+	public NewClientConnection(String serverIP, String clientPseudo, String password) {
 		this.serverIP = serverIP;
-		this.clientName = clientName;
+		this.clientPseudo = clientPseudo;
+		this.password = password;
 	}
 	
 	public void connectToServer() {
-		try {
-			System.out.println("Hello.");
-			
+		try {			
 			//TODO : scan des adresses IP qui écoutent sur le port 45000
 			serverAddress = InetAddress.getByName(serverIP);
 			System.out.println("I get the address of the server: " +serverAddress);
@@ -30,6 +34,12 @@ public class NewClientConnection {
 			System.out.println(clientSocket);
 			
 			System.out.println("I got the connexion to " +serverAddress);
+			
+			// Give login information to the server
+			dataOutput = new DataOutput(clientSocket);
+			dataOutput.giveInformationToServer(clientPseudo, password);		
+			
+			
 			System.out.println("Now dying...");
 			
 			clientSocket.close();
