802b27a1e3dbdccf440329527bb1857c7b7fe768
Buubu <montaine@windowslive.com>
2018-04-21 17:24:33 +0200

    Classes DataOuput and DataInput

---
+++ src/controller/DataInput.java
@@ -0,0 +1,32 @@
+package controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+
+public class DataInput {
+	private Socket socket;
+	private BufferedReader buffin;
+	
+	
+	public DataInput(Socket socket) {
+		this.socket = socket;
+	}
+	
+	
+	public String receiveInformationFromClient() {
+		try {
+			buffin = new BufferedReader (new InputStreamReader(socket.getInputStream()));
+			String message = buffin.readLine();
+			
+			buffin.close();
+			return message;			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
---
+++ src/controller/DataOutput.java
@@ -0,0 +1,49 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class DataOutput {
+	private Socket socket;
+	private PrintWriter pout;
+
+	public DataOutput(Socket socket) {
+		this.socket = socket;
+	}
+	
+	
+	public void giveInformationToServer(String clientPseudo, String password) {
+		String message = clientPseudo+ "|" +password;
+		
+		try {
+			pout = new PrintWriter(socket.getOutputStream());
+			
+			
+			// CHERCHE LES INFORMATIONS
+//			InetAddress address = InetAddress.getLocalHost();
+//	        String hostIP = address.getHostAddress() ;
+//	        String hostName = address.getHostName();
+			
+			// WAIT FOR AN INPUT FROM THE CONSOLE
+//	        sc = new Scanner(System.in);
+//	        System.out.println("Your message :");
+//	        String message = sc.nextLine();
+//			message += "hostIP: " + hostIP + "| hostName: " + hostName;
+			
+			
+			pout.println(message);
+			pout.flush();
+			
+			pout.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	
+	
+}
